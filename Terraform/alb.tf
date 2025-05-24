1ca48917393a2ac80bdae5ed37e1eb8a8ea4792c
Pratik Kshirsagar <106995514+pratik2135@users.noreply.github.com>
2025-05-24 20:02:15 +0530

    Update alb.tf
    Updated target group config.

--- Terraform/alb.tf
+++ Terraform/alb.tf
@@ -17,11 +17,12 @@ resource "aws_lb_target_group" "app_tg" {
     vpc_id   = module.vpc.vpc_id
 
     health_check {
-        path                = "/health"
+        path                = "/"
         interval            = 30
         timeout             = 5
         healthy_threshold  = 2
         unhealthy_threshold = 2
+        matcher             = "200"
     }
     target_type = "ip" # Use "ip" for Fargate tasks
 
